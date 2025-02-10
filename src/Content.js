2176d1cddef8d8d94b729200eb0b28cb43cd5c9d
marcmillie <mrmarcusmiller0@gmail.com>
2025-02-09 19:23:00 -0500

    second commit

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Content = () => {
+  const [name, setName] = useState('Zee');
+  const [count, setCount] = useState(0);
+
   const handleNameChange = () => {
     const names = ['Jireh', 'Sky', 'Zee'];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setName (names[int]);
   };
 
   const handleClick = () => {
-    console.log('Clicked')
+    console.log(count)
   }
 
   const handleClick2 = (name) => {
@@ -24,10 +28,10 @@ const Content = () => {
   return (
     <main>
       <p onDoubleClick={handleClick}>
-        Hello {handleNameChange()}!
+        Hello {name}!
       </p>
-      <button onClick={handleClick}>Click Me</button>
-      <button onClick={() => handleClick2('Zee')}>Click Me Too</button>
+      <button onClick={handleNameChange}>Click Me</button>
+      <button onClick={handleClick}>Click Me Too</button>
       <button onClick={(e) => handleClick3(e)}>Click Me as Well</button>
     </main>
   )
