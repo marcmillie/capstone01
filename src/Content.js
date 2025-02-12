8f5f0dd82d6ee6cbde8d4df3ecb21b0e579f3409
marcmillie <mrmarcusmiller0@gmail.com>
2025-02-11 20:00:13 -0500

    third commit

--- src/Content.js
+++ src/Content.js
@@ -2,37 +2,27 @@ import React from 'react'
 import { useState } from 'react';
 
 const Content = () => {
-  const [name, setName] = useState('Zee');
-  const [count, setCount] = useState(0);
-
-  const handleNameChange = () => {
-    const names = ['Jireh', 'Sky', 'Zee'];
-    const int = Math.floor(Math.random() * 3);
-    setName (names[int]);
-  };
-
-  const handleClick = () => {
-    console.log(count)
-  }
-
-  const handleClick2 = (name) => {
-    console.log(`${name} was clicked`)
-  }
-
-  const handleClick3 = (e) => {
-    console.log(e.target.innerText)
-  }
-
-
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "One half pound bag of Cocoa Almonds Unsalted"
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2"
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3"
+    }
+  ]);
 
   return (
     <main>
-      <p onDoubleClick={handleClick}>
-        Hello {name}!
-      </p>
-      <button onClick={handleNameChange}>Click Me</button>
-      <button onClick={handleClick}>Click Me Too</button>
-      <button onClick={(e) => handleClick3(e)}>Click Me as Well</button>
+     
     </main>
   )
 }
